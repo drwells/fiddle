a712a7f2b3fcf7654712972baf9ba020bfa0909a
David Wells <drwells@email.unc.edu>
2020-08-19 13:55:07 -0400

    Finish the serial proof of concept.
    We can transfer data between two distinct meshes that share cells.

--- fiddle.cc
+++ fiddle.cc
@@ -1,10 +1,19 @@
 #include <deal.II/base/mpi.h>
+#include <deal.II/base/index_set.h>
+#include <deal.II/base/function_lib.h>
+
+#include <deal.II/dofs/dof_handler.h>
+
+#include <deal.II/fe/fe_q.h>
 
 #include <deal.II/grid/tria.h>
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_out.h>
 
 #include <deal.II/distributed/shared_tria.h>
+
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/vector_tools.h>
 
 #include <iostream>
 #include <fstream>
@@ -41,28 +50,98 @@ bool intersects(const BoundingBox<spacedim> &a,
     return true;
 }
 
+// convenience class for converting between a local and global set of numbers.
+class IndexTranslator
+{
+  public:
+    IndexTranslator(const std::vector<types::global_dof_index> &consecutive_dofs,
+                    const std::vector<types::global_dof_index> &nonconsecutive_dofs)
+      : nonconsecutive_to_internal_index(nonconsecutive_dofs.size() == 0 ? 0  :
+                                         *std::max_element(nonconsecutive_dofs.begin(),
+                                                           nonconsecutive_dofs.end()))
+    {
+      Assert(consecutive_dofs.size() == nonconsecutive_dofs.size(),
+             ExcMessage("should have same length"));
+
+      // Set up the mapping from consecutive dofs to nonconsecutive dofs.
+      std::vector<std::pair<types::global_dof_index, types::global_dof_index>> temp;
+      for (std::size_t i = 0; i < consecutive_dofs.size(); ++i)
+        temp.emplace_back(consecutive_dofs[i], nonconsecutive_dofs[i]);
+      std::sort(temp.begin(), temp.end(),
+                [&](const auto &a, const auto &b)
+                {
+                  return a.first < b.first;
+                });
+      for (const auto &pair : temp) consecutive_to_non.push_back(pair.second);
+
+      // Set up the mapping from nonconsecutive to consecutive dofs.
+      std::sort(temp.begin(), temp.end(),
+                [](const auto &a, const auto &b)
+                {
+                  return a.second < b.second;
+                });
+      for (const auto &dof : nonconsecutive_dofs)
+        nonconsecutive_to_internal_index.add_index(dof);
+
+      nonconsecutive_to_internal_index.compress();
+      for (const auto &index : nonconsecutive_to_internal_index)
+      {
+        // Find the consecutive dof corresponding to the current nonconsecutive
+        // dof.
+        const auto it = std::lower_bound(
+          temp.begin(), temp.end(), index,
+          [](const std::pair<types::global_dof_index, types::global_dof_index> &a,
+             const types::global_dof_index &b)
+        {
+          return a.second < b;
+        });
+        // push back the entry for faster access in non_to_con:
+        internal_to_consecutive.push_back(it->first);
+      }
+    }
+
+    types::global_dof_index
+    con_to_non(const types::global_dof_index a)
+    {
+      Assert(a < consecutive_to_non.size(), ExcMessage("a too big"));
+      return consecutive_to_non[a];
+    }
+
+
+    types::global_dof_index
+    non_to_con(const types::global_dof_index b)
+    {
+      return internal_to_consecutive[nonconsecutive_to_internal_index.index_within_set(b)];
+    }
+
+
+  protected:
+    std::vector<types::global_dof_index> consecutive_to_non;
+
+    IndexSet nonconsecutive_to_internal_index;
+    std::vector<types::global_dof_index> internal_to_consecutive;
+};
+
+
 int main(int argc, char **argv)
 {
   Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
   const auto mpi_comm = MPI_COMM_WORLD;
   const auto rank = Utilities::MPI::this_mpi_process(mpi_comm);
   const types::subdomain_id local_native_subdomain_id = rank;
-  parallel::shared::Triangulation<2> tria_1(mpi_comm);
-  GridGenerator::hyper_ball(tria_1);
-  tria_1.refine_global(2);
+  parallel::shared::Triangulation<2> native_tria(mpi_comm);
+  GridGenerator::hyper_ball(native_tria);
+  native_tria.refine_global(2);
 
   {
     GridOut go;
     std::ofstream out("tria-1.eps");
-    go.write_eps(tria_1, out);
+    go.write_eps(native_tria, out);
   }
 
   {
     // TODO: this approach doesn't yet support triangulations on multiple
     // levels
-    Triangulation<2> tria_2;
-    std::vector<CellData<2>> cells;
-    SubCellData subcell_data;
     BoundingBox<2> bbox;
     switch (rank)
       {
@@ -86,8 +165,10 @@ int main(int argc, char **argv)
         Assert(false, ExcNotImplemented());
       }
 
+    std::vector<CellData<2>> cells;
     std::vector<Triangulation<2>::active_cell_iterator> intersecting_shared_cells;
-    for (const auto &cell : tria_1.active_cell_iterators())
+    SubCellData subcell_data;
+    for (const auto &cell : native_tria.active_cell_iterators())
       {
         if (intersects(cell->bounding_box(), bbox))
           {
@@ -115,9 +196,16 @@ int main(int argc, char **argv)
           }
       }
 
-    tria_2.create_triangulation(tria_1.get_vertices(), cells, SubCellData());
+    Triangulation<2> ib_tria;
+    ib_tria.create_triangulation(native_tria.get_vertices(), cells, SubCellData());
+    {
+      GridOut go;
+      std::ofstream out("tria-2-" + std::to_string(rank) + ".eps");
+      go.write_eps(ib_tria, out);
+    }
+
     // now fill in details:
-    for (auto &cell : tria_2.active_cell_iterators())
+    for (auto &cell : ib_tria.active_cell_iterators())
       {
         cell->set_user_index(cell->material_id());
         const auto &shared_cell = intersecting_shared_cells[cell->user_index()];
@@ -127,13 +215,15 @@ int main(int argc, char **argv)
       }
 
     // Get some other things that we need
-    std::vector<unsigned int> ib_cell_active_indices;
     std::set<unsigned int> relevant_subdomain_ids;
-    for (auto &cell : tria_2.active_cell_iterators())
+    std::map<unsigned int, std::vector<unsigned int>> subdomain_id_to_active_cell_index;
+    for (auto &cell : ib_tria.active_cell_iterators())
       {
         auto &native_cell = intersecting_shared_cells[cell->user_index()];
+        const auto subdomain_id = native_cell->subdomain_id();
         relevant_subdomain_ids.insert(native_cell->subdomain_id());
-        ib_cell_active_indices.push_back(native_cell->active_cell_index());
+        subdomain_id_to_active_cell_index[subdomain_id].push_back
+          (native_cell->active_cell_index());
       }
 
     // OK, now we know which cells we need to get dof data from
@@ -142,7 +232,6 @@ int main(int argc, char **argv)
     // 1. Figure out what processors we need data from (done in the loop above)
     // 2. Figure out what processors need data from us (done by
     //    compute_point_to_point_communication_pattern)
-
     relevant_subdomain_ids.erase(local_native_subdomain_id);
     const std::vector<unsigned int> native_procs_used_on_local_ib
       (relevant_subdomain_ids.begin(), relevant_subdomain_ids.end());
@@ -150,18 +239,133 @@ int main(int argc, char **argv)
       Utilities::MPI::compute_point_to_point_communication_pattern
       (mpi_comm, native_procs_used_on_local_ib);
 
-    // We know which processors need our dofs and which processors have our
-    // dofs.
-    //
-    // 1. Define some packed format consisting of active cell index, n_dofs,
-    //    and dofs themselves.
-    // 2. From the packed format calculate the data length required by other
-    //    processors and communicate that to receiving processors.
-    // 3. Send the dof data.
+    FE_Q<2> fe(3);
+    DoFHandler<2> native_dof_handler(native_tria);
+    native_dof_handler.distribute_dofs(fe);
+    DoFHandler<2> ib_dof_handler(ib_tria);
+    ib_dof_handler.distribute_dofs(fe);
+
+    // generate some data so we can check what happens when we move
+    Vector<double> native_solution(native_dof_handler.n_dofs());
+    VectorTools::interpolate(native_dof_handler,
+                             Functions::CosineFunction<2>(),
+                             native_solution);
+
+    // go for something even easier - set up the mapping on a single processor first
+    Assert(rank == 0, ExcNotImplemented());
+
+    std::vector<unsigned int> native_active_cell_ids_on_ib;
+    for (const auto &cell : ib_tria.active_cell_iterators())
+      {
+        auto &native_cell = intersecting_shared_cells[cell->user_index()];
+        native_active_cell_ids_on_ib.push_back(native_cell->active_cell_index());
+      }
+    std::sort(native_active_cell_ids_on_ib.begin(),
+              native_active_cell_ids_on_ib.end());
+    std::vector<types::global_dof_index> dofs_on_native;
+
+    // Extract the requested dofs
     {
-      GridOut go;
-      std::ofstream out("tria-2-" + std::to_string(rank) + ".svg");
-      go.write_svg(tria_2, out);
+      std::vector<DoFHandler<2>::active_cell_iterator> cell_iterators;
+      for (const auto &cell : native_dof_handler.active_cell_iterators())
+        cell_iterators.push_back(cell);
+      std::sort(cell_iterators.begin(), cell_iterators.end(),
+                [](const auto &a, const auto &b){return a->active_cell_index()
+                    < b->active_cell_index();});
+
+      auto active_cell_index_ptr = native_active_cell_ids_on_ib.begin();
+      std::vector<types::global_dof_index> cell_dofs(fe.dofs_per_cell);
+      // there are at least as many cells as requested cells
+      for (const auto &cell : cell_iterators)
+      {
+        if (cell->active_cell_index() != *active_cell_index_ptr)
+          continue;
+
+        Assert(active_cell_index_ptr < native_active_cell_ids_on_ib.end(),
+               ExcInternalError());
+        dofs_on_native.push_back(*active_cell_index_ptr);
+        cell->get_dof_indices(cell_dofs);
+        dofs_on_native.push_back(cell_dofs.size());
+          for (const auto dof : cell_dofs)
+            dofs_on_native.push_back(dof);
+          // include a sentinel
+          dofs_on_native.push_back(numbers::invalid_dof_index);
+
+        ++active_cell_index_ptr;
+      }
+    }
+
+    // todo - when we support multiple processors we will need some merge sort
+    // step in here to combine things into a single giant packed array
+
+    // we now have the native dofs on each cell in a packed format: active cell
+    // index, dofs, sentinel.
+
+    std::vector<types::global_dof_index> cell_dofs(fe.dofs_per_cell);
+    {
+      std::vector<DoFHandler<2>::active_cell_iterator> active_cells;
+      std::vector<types::global_dof_index> ib_dofs;
+      std::vector<types::global_dof_index> native_dofs;
+
+      for (const auto &cell : ib_dof_handler.active_cell_iterators())
+        active_cells.push_back(cell);
+
+      // sort by active cell indices of the native triangulation:
+      std::sort(active_cells.begin(), active_cells.end(),
+                [&](const auto &a, const auto &b){return
+                    intersecting_shared_cells[a->user_index()]->active_cell_index() <
+                      intersecting_shared_cells[b->user_index()]->active_cell_index();});
+
+      auto packed_ptr = dofs_on_native.begin();
+      std::vector<types::global_dof_index> native_cell_dofs(fe.dofs_per_cell);
+      std::vector<types::global_dof_index> ib_cell_dofs(fe.dofs_per_cell);
+      Vector<double> ib_solution(ib_dof_handler.n_dofs());
+      for (const auto &cell : active_cells)
+      {
+        auto &native_cell = intersecting_shared_cells[cell->user_index()];
+        Assert(*packed_ptr == native_cell->active_cell_index(), ExcInternalError());
+        ++packed_ptr;
+        const auto n_dofs = *packed_ptr;
+        ++packed_ptr;
+
+        std::cout << "dofs on native:\n";
+        native_cell_dofs.clear();
+        for (unsigned int i = 0; i < n_dofs; ++i)
+        {
+          std::cout << *packed_ptr << '\n';
+          native_cell_dofs.push_back(*packed_ptr);
+          ++packed_ptr;
+        }
+        Assert(*packed_ptr == numbers::invalid_dof_index, ExcInternalError());
+        ++packed_ptr;
+
+        cell->get_dof_indices(ib_cell_dofs);
+        for (unsigned int i = 0; i < ib_cell_dofs.size(); ++i)
+        {
+          ib_solution[ib_cell_dofs[i]] = native_solution[native_cell_dofs[i]];
+        }
+      }
+
+
+      {
+        DataOut<2> data_out;
+        data_out.attach_dof_handler(native_dof_handler);
+        data_out.add_data_vector(native_solution, "solution");
+        data_out.build_patches();
+
+        std::ofstream out("native-solution.vtu");
+        data_out.write_vtu(out);
+      }
+
+      {
+        DataOut<2> data_out;
+        data_out.attach_dof_handler(ib_dof_handler);
+        data_out.add_data_vector(ib_solution, "solution");
+        data_out.build_patches();
+
+        std::ofstream out("ib-solution.vtu");
+        data_out.write_vtu(out);
+      }
     }
   }
 }
