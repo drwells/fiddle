028a08e7bb57d904fd9c3204d9da1ecdbd978514
David Wells <drwells@email.unc.edu>
2021-06-06 10:59:51 -0400

    Make the intersecting sphere code a lot faster.
    We should do a similar trick in 3D but this is probably good enough for now.

--- include/fiddle/base/utilities.h
+++ include/fiddle/base/utilities.h
@@ -3,7 +3,9 @@
 
 #include <deal.II/base/point.h>
 
+#include <algorithm>
 #include <utility>
+#include <vector>
 
 namespace fdl
 {
@@ -25,23 +27,66 @@ namespace fdl
     Point<spacedim> best_center;
     double          best_diameter = 0.0;
 
+    // Speed up the bounding check by only examining points whose x coordinate
+    // is near that of the sphere
+    auto x_less = [](const Point<spacedim> &a, const Point<spacedim> &b) {
+      return a[0] < b[0];
+    };
+
+    std::vector<Point<spacedim>> points_copy(points);
+    std::sort(points_copy.begin(), points_copy.end(), x_less);
+
     // This is the brute-force version. We don't normally have more than a
     // hundred quadrature points per cell, so N^2 algorithms aren't
     // completely deadly yet.
     auto sphere_contains_nontangent_point =
-      [&points](const Point<spacedim> &center,
-                const Point<spacedim> &tangent_point_1,
-                const Point<spacedim> &tangent_point_2,
-                const double           diameter) -> bool {
-      const double magnitude = std::max(center.norm(), tangent_point_1.norm());
-      for (const auto &point : points)
-        if ((point != tangent_point_1) && (point != tangent_point_2) &&
-            // relax the check a little bit by ensuring that the point
-            // is actually inside the sphere with some tolerance
-            (point.distance(center) - diameter / 2.0) < -magnitude * 1e-14)
+      [&](const Point<spacedim> &center,
+          const unsigned int &   tangent_point_1_n,
+          const unsigned int &   tangent_point_2_n,
+          const double           diameter) -> bool {
+      const double magnitude =
+        std::max(center.norm(), points[tangent_point_1_n].norm());
+
+      Point<spacedim> left  = center;
+      Point<spacedim> right = center;
+      left[0] -= diameter / 2.0;
+      right[0] += diameter / 2.0;
+
+      // Only check points that are in the correct x pencil.
+      auto upper_bound =
+        std::lower_bound(points_copy.begin(), points_copy.end(), right, x_less);
+      auto lower_bound =
+        std::lower_bound(points_copy.begin(), points_copy.end(), left, x_less);
+      if (lower_bound != points_copy.begin())
+        --lower_bound;
+
+      for (auto it = lower_bound; it < upper_bound; ++it)
+        if ((it->distance(center) - diameter / 2.0) < -magnitude * 1e-14)
           return true;
+
       return false;
     };
+
+    // points tend to be clustered at endpoints. Try to find one in the middle
+    // to start with:
+    {
+      const unsigned int i      = points.size() / 2;
+      const auto &       point1 = points[i];
+      for (unsigned int j = i + 1; j < points.size(); ++j)
+        {
+          const auto &          point2             = points[j];
+          const Point<spacedim> tentative_center   = (point1 + point2) / 2.0;
+          const double          tentative_diameter = point1.distance(point2);
+
+          if ((tentative_diameter > best_diameter) &&
+              !sphere_contains_nontangent_point(
+                tentative_center, i, j, tentative_diameter))
+            {
+              best_center   = tentative_center;
+              best_diameter = tentative_diameter;
+            }
+        }
+    }
 
     for (unsigned int i = 0; i < points.size(); ++i)
       {
@@ -54,7 +99,7 @@ namespace fdl
 
             if ((tentative_diameter > best_diameter) &&
                 !sphere_contains_nontangent_point(
-                  tentative_center, point1, point2, tentative_diameter))
+                  tentative_center, i, j, tentative_diameter))
               {
                 best_center   = tentative_center;
                 best_diameter = tentative_diameter;
