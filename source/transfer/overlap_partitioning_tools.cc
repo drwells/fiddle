dc57637cb60ea3b0dc536968132ed8b7438b1356
David Wells <drwells@email.unc.edu>
2021-06-20 13:43:43 -0400

    Remove ifdefs.

--- source/transfer/overlap_partitioning_tools.cc
+++ source/transfer/overlap_partitioning_tools.cc
@@ -97,83 +97,16 @@ namespace fdl
         Utilities::MPI::some_to_some(mpi_comm, dofs_on_native);
 
     // we now have the native dofs on each cell in a packed format: active cell
-    // index, dofs, sentinel. Make it easier to look up local cells by sorting
-    // by global active cell indices:
-#if 0
-    std::vector<typename DoFHandler<dim, spacedim>::active_cell_iterator>
-      overlap_dh_cells;
-    for (const auto &cell : overlap_dof_handler.active_cell_iterators())
-      {
-        if (cell->is_locally_owned())
-          overlap_dh_cells.push_back(cell);
-      }
-    std::sort(overlap_dh_cells.begin(),
-              overlap_dh_cells.end(),
-              [&](const auto &a, const auto &b) {
-                return overlap_tria.get_native_cell(a)->active_cell_index() <
-                       overlap_tria.get_native_cell(b)->active_cell_index();
-              });
-#else
+    // index, dofs, sentinel. Read dof data back in the order in which it was
+    // originally requested:
     std::map<types::subdomain_id, std::vector<types::global_dof_index>::const_iterator>
       packed_ptrs;
     for (const auto &pair : native_dof_indices)
       packed_ptrs[pair.first] = pair.second.cbegin();
-#endif
 
     // 4:
     std::vector<std::pair<types::global_dof_index, types::global_dof_index>>
       overlap_to_native;
-#if 0
-    for (const auto &pair : native_dof_indices)
-      {
-        const std::vector<types::global_dof_index> &native_dofs = pair.second;
-        auto                                 packed_ptr = native_dofs.cbegin();
-        std::vector<types::global_dof_index> native_cell_dofs(fe.dofs_per_cell);
-        std::vector<types::global_dof_index> overlap_cell_dofs(
-          fe.dofs_per_cell);
-
-        while (packed_ptr < native_dofs.cend())
-          {
-            const auto active_cell_index = *packed_ptr;
-            ++packed_ptr;
-            // Find the overlap cell corresponding to the given active cell
-            // index.
-            const auto overlap_dh_cell = std::lower_bound(
-              overlap_dh_cells.begin(),
-              overlap_dh_cells.end(),
-              active_cell_index,
-              [&](const auto &a, const auto &val) {
-                return overlap_tria.get_native_cell(a)->active_cell_index() <
-                       val;
-              });
-            Assert(overlap_dh_cell != overlap_dh_cells.end(),
-                   ExcFDLInternalError());
-            Assert(overlap_tria.get_native_cell(*overlap_dh_cell)
-                       ->active_cell_index() == active_cell_index,
-                   ExcFDLInternalError());
-            const auto n_dofs = *packed_ptr;
-            ++packed_ptr;
-
-            native_cell_dofs.clear();
-            for (unsigned int i = 0; i < n_dofs; ++i)
-              {
-                native_cell_dofs.push_back(*packed_ptr);
-                ++packed_ptr;
-              }
-            Assert(*packed_ptr == numbers::invalid_dof_index,
-                   ExcFDLInternalError());
-            ++packed_ptr;
-
-            // Copy data between orderings.
-            (*overlap_dh_cell)->get_dof_indices(overlap_cell_dofs);
-            for (unsigned int i = 0; i < n_dofs; ++i)
-              {
-                overlap_to_native.emplace_back(overlap_cell_dofs[i],
-                                               native_cell_dofs[i]);
-              }
-          }
-      }
-#else
     std::vector<types::global_dof_index> native_cell_dofs(fe.dofs_per_cell);
     std::vector<types::global_dof_index> overlap_cell_dofs(fe.dofs_per_cell);
     for (const auto &cell : overlap_dof_handler.active_cell_iterators())
@@ -212,7 +145,6 @@ namespace fdl
                                            native_cell_dofs[i]);
         }
       }
-#endif
     std::sort(overlap_to_native.begin(),
               overlap_to_native.end(),
               [](const auto &a, const auto &b) { return a.first < b.first; });
