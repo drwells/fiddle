aec22be0921164b9572c741480a75066bc80c0ef
David Wells <drwells@email.unc.edu>
2021-09-27 13:50:44 -0400

    Implement SpringForce.
    A basic restorative force with a spring constant. Useful in examples and also as
    a building block for DLM.

---
+++ source/mechanics/force_contribution_lib.cc
@@ -0,0 +1,113 @@
+#include <fiddle/base/exceptions.h>
+
+#include <fiddle/mechanics/force_contribution_lib.h>
+
+#include <deal.II/lac/la_parallel_vector.h>
+
+#include <algorithm>
+
+namespace fdl
+{
+  using namespace dealii;
+
+  template <int dim, int spacedim, typename Number>
+  SpringForce<dim, spacedim, Number>::SpringForce(
+    const Quadrature<dim> &                           quad,
+    const double                                      spring_constant,
+    const DoFHandler<dim, spacedim> &                 dof_handler,
+    const LinearAlgebra::distributed::Vector<double> &reference_position)
+    : ForceContribution<dim, spacedim, double>(quad)
+    , spring_constant(spring_constant)
+    , dof_handler(&dof_handler)
+    , reference_position(reference_position)
+  {
+    this->reference_position.update_ghost_values();
+  }
+
+  template <int dim, int spacedim, typename Number>
+  void
+  SpringForce<dim, spacedim, Number>::set_reference_position(
+    const LinearAlgebra::distributed::Vector<double> &reference_position)
+  {
+    this->reference_position = reference_position;
+    this->reference_position.update_ghost_values();
+  }
+
+
+  template <int dim, int spacedim, typename Number>
+  MechanicsUpdateFlags
+  SpringForce<dim, spacedim, Number>::get_mechanics_update_flags() const
+  {
+    return MechanicsUpdateFlags::update_nothing;
+  }
+
+  template <int dim, int spacedim, typename Number>
+  UpdateFlags
+  SpringForce<dim, spacedim, Number>::get_update_flags() const
+  {
+    return UpdateFlags::update_values;
+  }
+
+  template <int dim, int spacedim, typename Number>
+  bool
+  SpringForce<dim, spacedim, Number>::is_volume_force() const
+  {
+    return true;
+  }
+
+  template <int dim, int spacedim, typename Number>
+  void
+  SpringForce<dim, spacedim, Number>::setup_force(
+    const double /*time*/,
+    const LinearAlgebra::distributed::Vector<double> &position,
+    const LinearAlgebra::distributed::Vector<double> & /*velocity*/)
+  {
+    current_position = &position;
+  }
+
+  template <int dim, int spacedim, typename Number>
+  void
+  SpringForce<dim, spacedim, Number>::finish_force(const double /*time*/)
+  {
+    current_position = nullptr;
+  }
+
+  template <int dim, int spacedim, typename Number>
+  void
+  SpringForce<dim, spacedim, Number>::compute_volume_force(
+    const double /*time*/,
+    const MechanicsValues<dim, spacedim> &                             m_values,
+    const typename Triangulation<dim, spacedim>::active_cell_iterator &cell,
+    ArrayView<Tensor<1, spacedim, Number>> &forces) const
+  {
+    const FEValuesBase<dim, spacedim> &fe_values = m_values.get_fe_values();
+
+    const auto dof_cell =
+      typename DoFHandler<dim, spacedim>::active_cell_iterator(
+        &dof_handler->get_triangulation(),
+        cell->level(),
+        cell->index(),
+        &*dof_handler);
+
+    scratch_cell_dofs.resize(fe_values.dofs_per_cell);
+    dof_cell->get_dof_indices(scratch_cell_dofs);
+    scratch_dof_values.resize(fe_values.dofs_per_cell);
+    scratch_qp_values.resize(fe_values.n_quadrature_points);
+
+    auto &extractor = fe_values[FEValuesExtractors::Vector(0)];
+    for (unsigned int i = 0; i < scratch_cell_dofs.size(); ++i)
+      scratch_dof_values[i] =
+        spring_constant * (reference_position[scratch_cell_dofs[i]] -
+                           (*current_position)[scratch_cell_dofs[i]]);
+    extractor.get_function_values_from_local_dof_values(scratch_dof_values,
+                                                        scratch_qp_values);
+    std::copy(scratch_qp_values.begin(),
+              scratch_qp_values.end(),
+              forces.begin());
+  }
+
+
+
+  template class SpringForce<NDIM - 1, NDIM, double>;
+  template class SpringForce<NDIM, NDIM, double>;
+} // namespace fdl
---
+++ tests/mechanics/spring_01.cc
@@ -0,0 +1,111 @@
+#include <fiddle/mechanics/force_contribution_lib.h>
+#include <fiddle/mechanics/mechanics_values.h>
+
+#include <deal.II/base/function_lib.h>
+#include <deal.II/base/quadrature_lib.h>
+
+#include <deal.II/dofs/dof_handler.h>
+
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_system.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/tria.h>
+
+#include <deal.II/lac/la_parallel_vector.h>
+
+#include <deal.II/numerics/vector_tools_interpolate.h>
+
+#include <fstream>
+#include <vector>
+
+// Basic test for SpringForce
+
+using namespace dealii;
+template <int dim>
+class Shift : public Function<dim>
+{
+public:
+  Shift()
+    : Function<dim>(dim)
+  {}
+
+  virtual double
+  value(const Point<dim> &p, const unsigned int component = 0) const override
+  {
+    AssertIndexRange(component, dim);
+    return p[component] + p[0];
+  }
+};
+
+
+int
+main()
+{
+  Triangulation<2> tria;
+  GridGenerator::hyper_cube(tria);
+  tria.refine_global(1);
+  FESystem<2>   fe(FE_Q<2>(1), 2);
+  DoFHandler<2> dof_handler(tria);
+  dof_handler.distribute_dofs(fe);
+
+  const MPI_Comm comm = MPI_COMM_WORLD;
+
+  LinearAlgebra::distributed::Vector<double> reference(
+    dof_handler.locally_owned_dofs(), comm);
+  VectorTools::interpolate(dof_handler,
+                           Functions::IdentityFunction<2>(),
+                           reference);
+
+  LinearAlgebra::distributed::Vector<double> current(
+    dof_handler.locally_owned_dofs(), comm);
+  for (std::size_t i = 0; i < current.locally_owned_size(); ++i)
+    current[i] = 2.0 + reference[i];
+
+  // We have to do some manual setup of stuff normally done inside the library
+  QMidpoint<2>            quadrature;
+  FEValues<2>             fe_values(fe, quadrature, update_values);
+  fdl::MechanicsValues<2> m_values(fe_values,
+                                   current,
+                                   current,
+                                   fdl::MechanicsUpdateFlags::update_nothing);
+
+  const double        spring_constant = 10.0;
+  fdl::SpringForce<2> spring_force(quadrature,
+                                   spring_constant,
+                                   dof_handler,
+                                   current);
+  spring_force.set_reference_position(reference);
+
+  spring_force.setup_force(0.0, current, current);
+  std::vector<Tensor<1, 2>> forces(quadrature.size());
+  std::ofstream             output("output");
+  output << "test 1: displace 2 forward, spring constant = " << spring_constant
+         << '\n';
+  for (const auto &cell : dof_handler.active_cell_iterators())
+    {
+      // more manual stuff that normally would not be necessary
+      fe_values.reinit(cell);
+      output << "cell center = " << cell->center() << '\n';
+      auto view = make_array_view(forces);
+      spring_force.compute_volume_force(0.0, m_values, cell, view);
+      output << "forces = ";
+      for (const Tensor<1, 2> &f : forces)
+        output << f << '\n';
+    }
+
+  VectorTools::interpolate(dof_handler, Shift<2>(), current);
+  output << "test 2: displace + x forward, spring constant = "
+         << spring_constant << '\n';
+  for (const auto &cell : dof_handler.active_cell_iterators())
+    {
+      // more manual stuff that normally would not be necessary
+      fe_values.reinit(cell);
+      output << "cell center = " << cell->center() << '\n';
+      auto view = make_array_view(forces);
+      spring_force.compute_volume_force(0.0, m_values, cell, view);
+      output << "forces = ";
+      for (const Tensor<1, 2> &f : forces)
+        output << f << '\n';
+    }
+}
