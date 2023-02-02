a247b47ab57eaed27a9bc7d76185e28cdf2d51db
David Wells <drwells@email.unc.edu>
2023-02-02 14:49:06 -0500

    Get multilevel nodal interaction working with general nodal elements.

--- source/interaction/nodal_interaction.cc
+++ source/interaction/nodal_interaction.cc
@@ -1,13 +1,16 @@
 #include <fiddle/base/samrai_utilities.h>
 
+#include <fiddle/grid/box_utilities.h>
+
 #include <fiddle/interaction/interaction_utilities.h>
 #include <fiddle/interaction/nodal_interaction.h>
 
 #include <fiddle/transfer/overlap_partitioning_tools.h>
 
-#include <fiddle/grid/box_utilities.h>
-
 #include <deal.II/dofs/dof_renumbering.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/mapping_fe_field.h>
 
 #include <CartesianPatchGeometry.h>
 
@@ -19,19 +22,51 @@ namespace fdl
   using namespace dealii;
   using namespace SAMRAI;
 
+  namespace
+  {
+    // Helper function for computing the locations of nodes for DoFHandlers
+    // with different base elements from the position DoFHandler.
+    template <int dim, int spacedim>
+    Vector<double>
+    compute_nodes(const DoFHandler<dim, spacedim> &position_dof_handler,
+                  const Vector<double> &           position,
+                  const DoFHandler<dim, spacedim> &dof_handler)
+    {
+      MappingFEField<dim, spacedim, Vector<double>> position_mapping(
+        position_dof_handler, position);
+      std::vector<Point<spacedim>> support_points(dof_handler.n_dofs());
+      DoFTools::map_dofs_to_support_points(position_mapping,
+                                           dof_handler,
+                                           support_points);
+      support_points.erase(std::unique(support_points.begin(),
+                                       support_points.end()),
+                           support_points.end());
+      Assert(dof_handler.n_dofs() ==
+               support_points.size() * dof_handler.get_fe().n_components(),
+             ExcFDLInternalError());
+
+      Vector<double> nodal_coordinates(support_points.size() * spacedim);
+      for (unsigned int node_n = 0; node_n < support_points.size(); ++node_n)
+        for (unsigned int d = 0; d < spacedim; ++d)
+          nodal_coordinates[node_n * spacedim + d] = support_points[node_n][d];
+
+      return nodal_coordinates;
+    }
+  } // namespace
+
   template <int dim, int spacedim>
   NodalInteraction<dim, spacedim>::NodalInteraction()
   {}
 
   template <int dim, int spacedim>
   NodalInteraction<dim, spacedim>::NodalInteraction(
-    const tbox::Pointer<tbox::Database>                  &input_db,
+    const tbox::Pointer<tbox::Database> &                 input_db,
     const parallel::shared::Triangulation<dim, spacedim> &native_tria,
-    const std::vector<BoundingBox<spacedim, float>>      &active_cell_bboxes,
+    const std::vector<BoundingBox<spacedim, float>> &     active_cell_bboxes,
     tbox::Pointer<hier::BasePatchHierarchy<spacedim>>     patch_hierarchy,
-    const std::pair<int, int>                            &level_numbers,
-    const DoFHandler<dim, spacedim>                      &position_dof_handler,
-    const LinearAlgebra::distributed::Vector<double>     &position)
+    const std::pair<int, int> &                           level_numbers,
+    const DoFHandler<dim, spacedim> &                     position_dof_handler,
+    const LinearAlgebra::distributed::Vector<double> &    position)
   {
     reinit(input_db,
            native_tria,
@@ -89,65 +124,77 @@ namespace fdl
       this->return_scatter(position_dof_handler, std::move(scatter));
     }
 
-    {
-      const double ghost_cell_fraction =
-        input_db->getDoubleWithDefault("ghost_cell_fraction", 1.0);
+    const double ghost_cell_fraction =
+      input_db->getDoubleWithDefault("ghost_cell_fraction", 1.0);
 
-      // This won't work correctly yet with no ghost cell fraction
-      AssertThrow(ghost_cell_fraction > 0.0, ExcFDLNotImplemented());
+    // This won't work correctly yet with no ghost cell fraction
+    AssertThrow(ghost_cell_fraction > 0.0, ExcFDLNotImplemented());
 
-      std::vector<tbox::Pointer<hier::Patch<spacedim>>> patches;
-      std::vector<std::vector<BoundingBox<spacedim>>>   bboxes;
-      // add boxes which do not intersect the next finest patch level...
-      for (int ln = level_numbers.first; ln < level_numbers.second; ++ln)
-        {
-          const auto patch_level       = patch_hierarchy->getPatchLevel(ln);
-          const auto level_patches     = extract_patches(patch_level);
-          const auto level_patch_boxes = compute_nonoverlapping_patch_boxes(
-            patch_level, patch_hierarchy->getPatchLevel(ln + 1));
-          AssertDimension(level_patches.size(), level_patch_boxes.size());
-
-          patches.insert(patches.end(),
-                         level_patches.begin(),
-                         level_patches.end());
-          for (const auto &patch_boxes : level_patch_boxes)
-            {
-              bboxes.emplace_back();
-              for (const auto &box : patch_boxes)
-                bboxes.back().push_back(box_to_bbox(box, patch_level));
-            }
-        }
-      // ... and boxes on the finest patch level.
-      const auto patch_level =
-        patch_hierarchy->getPatchLevel(level_numbers.second);
-      const auto level_patches =
-        extract_patches(patch_hierarchy->getPatchLevel(level_numbers.second));
-      patches.insert(patches.end(), level_patches.begin(), level_patches.end());
-      for (const auto &patch : level_patches)
-        {
-          bboxes.emplace_back();
-          bboxes.back().push_back(box_to_bbox(patch->getBox(), patch_level));
-        }
-
-      // Increase all the boxes by the ghost cell fraction:
+    std::vector<tbox::Pointer<hier::Patch<spacedim>>> patches;
+    std::vector<std::vector<BoundingBox<spacedim>>>   bboxes;
+    // add boxes which do not intersect the next finest patch level...
+    for (int ln = level_numbers.first; ln < level_numbers.second; ++ln)
       {
-        double patch_dx_min = std::numeric_limits<double>::max();
-        if (patches.size() > 0)
+        const auto patch_level       = patch_hierarchy->getPatchLevel(ln);
+        const auto level_patches     = extract_patches(patch_level);
+        const auto level_patch_boxes = compute_nonoverlapping_patch_boxes(
+          patch_level, patch_hierarchy->getPatchLevel(ln + 1));
+        AssertDimension(level_patches.size(), level_patch_boxes.size());
+
+        patches.insert(patches.end(),
+                       level_patches.begin(),
+                       level_patches.end());
+        for (const auto &patch_boxes : level_patch_boxes)
           {
-            const tbox::Pointer<geom::CartesianPatchGeometry<spacedim>>
-              geometry = patches.back()->getPatchGeometry();
-            Assert(geometry, ExcFDLNotImplemented());
-            const double *const patch_dx = geometry->getDx();
-            patch_dx_min = *std::min_element(patch_dx, patch_dx + spacedim);
+            bboxes.emplace_back();
+            for (const auto &box : patch_boxes)
+              bboxes.back().push_back(box_to_bbox(box, patch_level));
           }
-        for (auto &vec : bboxes)
-          for (auto &box : vec)
-            box.extend(patch_dx_min * ghost_cell_fraction);
+      }
+    // ... and boxes on the finest patch level.
+    const auto patch_level =
+      patch_hierarchy->getPatchLevel(level_numbers.second);
+    const auto level_patches =
+      extract_patches(patch_hierarchy->getPatchLevel(level_numbers.second));
+    patches.insert(patches.end(), level_patches.begin(), level_patches.end());
+    for (const auto &patch : level_patches)
+      {
+        bboxes.emplace_back();
+        bboxes.back().push_back(box_to_bbox(patch->getBox(), patch_level));
       }
 
-      nodal_patch_map.reinit(patches, bboxes, overlap_position);
-      // TODO: we should save patches and bboxes for later to set up any
-      // additional DoFHandlers which use different nodes
+    // Increase all the boxes by the ghost cell fraction:
+    {
+      double patch_dx_min = std::numeric_limits<double>::max();
+      if (patches.size() > 0)
+        {
+          const tbox::Pointer<geom::CartesianPatchGeometry<spacedim>> geometry =
+            patches.back()->getPatchGeometry();
+          Assert(geometry, ExcFDLNotImplemented());
+          const double *const patch_dx = geometry->getDx();
+          patch_dx_min = *std::min_element(patch_dx, patch_dx + spacedim);
+        }
+      for (auto &vec : bboxes)
+        for (auto &box : vec)
+          box.extend(patch_dx_min * ghost_cell_fraction);
+    }
+
+    // Set up class members:
+    {
+      nodal_patch_maps.resize(0);
+      // position dof handler is always at index 0
+      Assert(this->native_dof_handlers.size() == 1 &&
+               &*this->native_dof_handlers[0] == &position_dof_handler,
+             ExcFDLInternalError());
+      nodal_patch_maps.push_back(
+        std::make_shared<NodalPatchMap<dim, spacedim>>(patches,
+                                                       bboxes,
+                                                       overlap_position));
+
+      this->native_position_dof_handler = &position_dof_handler;
+      this->overlap_position            = std::move(overlap_position);
+      this->patches                     = std::move(patches);
+      this->bboxes                      = std::move(bboxes);
     }
   }
 
@@ -156,8 +203,13 @@ namespace fdl
   NodalInteraction<dim, spacedim>::add_dof_handler(
     const DoFHandler<dim, spacedim> &native_dof_handler)
   {
-    // This is basically a cut-and-paste of the base class method, except that
-    // we need to renumber the dofs halfway through.
+    // we only support vector-valued elements which have exactly one base
+    // element
+    AssertThrow(native_dof_handler.get_fe().n_base_elements() == 1,
+                ExcFDLNotImplemented());
+
+    // The rest is basically a cut-and-paste of the base class method, except
+    // that we need to renumber the dofs halfway through.
     AssertThrow(&native_dof_handler.get_triangulation() == this->native_tria,
                 ExcMessage("The DoFHandler must use the underlying native "
                            "triangulation."));
@@ -242,11 +294,26 @@ namespace fdl
     trans.position_scatter.global_to_overlap_finish(*trans.native_position,
                                                     trans.overlap_position);
 
+    // If needed, convert the given position vector into the relevant nodal
+    // one
+    const bool reuse_nodes =
+      trans.native_position_dof_handler->get_fe().base_element(0) ==
+      trans.native_dof_handler->get_fe().base_element(0);
+    Vector<double> nodal_coordinates;
+    if (!reuse_nodes)
+      {
+        nodal_coordinates = compute_nodes(
+          this->get_overlap_dof_handler(*trans.native_position_dof_handler),
+          trans.overlap_position,
+          this->get_overlap_dof_handler(*trans.native_dof_handler));
+      }
+
     // Actually do the work:
     compute_nodal_interpolation(trans.kernel_name,
                                 trans.current_data_idx,
-                                nodal_patch_map,
-                                trans.overlap_position,
+                                get_nodal_patch_map(*trans.native_dof_handler),
+                                reuse_nodes ? trans.overlap_position :
+                                              nodal_coordinates,
                                 trans.overlap_rhs);
 
     // After we compute we begin the scatter back to the native partitioning:
@@ -288,7 +355,9 @@ namespace fdl
     // Actually do the spreading:
     compute_nodal_spread(trans.kernel_name,
                          trans.current_data_idx,
-                         nodal_patch_map,
+                         // TODO: casting away const is bad
+                         const_cast<NodalPatchMap<dim, spacedim> &>(
+                           get_nodal_patch_map(*trans.native_dof_handler)),
                          trans.overlap_position,
                          trans.overlap_solution);
 
@@ -311,7 +380,11 @@ namespace fdl
     trans.position_scatter.global_to_overlap_finish(*trans.native_position,
                                                     trans.overlap_position);
 
-    count_nodes(trans.workload_index, nodal_patch_map, trans.overlap_position);
+    count_nodes(trans.workload_index,
+                // TODO: casting away const is bad
+                const_cast<NodalPatchMap<dim, spacedim> &>(
+                  get_nodal_patch_map(*trans.native_position_dof_handler)),
+                trans.overlap_position);
 
     trans.next_state = WorkloadTransaction<dim, spacedim>::State::Finish;
 
@@ -327,6 +400,47 @@ namespace fdl
   }
 
 
+
+  template <int dim, int spacedim>
+  const NodalPatchMap<dim, spacedim> &
+  NodalInteraction<dim, spacedim>::get_nodal_patch_map(
+    const DoFHandler<dim, spacedim> &native_dof_handler) const
+  {
+    auto iter = std::find(this->native_dof_handlers.begin(),
+                          this->native_dof_handlers.end(),
+                          &native_dof_handler);
+    AssertThrow(iter != this->native_dof_handlers.end(),
+                ExcMessage("The provided dof handler must already be "
+                           "registered with this class."));
+
+    const std::size_t index = iter - this->native_dof_handlers.begin();
+    if (nodal_patch_maps.size() <= index)
+      nodal_patch_maps.resize(index + 1);
+    if (nodal_patch_maps[index] != nullptr)
+      return *nodal_patch_maps[index];
+    else if (native_dof_handler.get_fe().base_element(0) ==
+             native_position_dof_handler->get_fe().base_element(0))
+      {
+        // if the base elements are the same, then we can reuse the position
+        // patch map, regardless of the number of components
+        nodal_patch_maps[index] = nodal_patch_maps[0];
+      }
+    else
+      {
+        // otherwise find the locations of the nodes and go from there
+        const auto nodal_coordinates =
+          compute_nodes(this->get_overlap_dof_handler(
+                          *native_position_dof_handler),
+                        overlap_position,
+                        this->get_overlap_dof_handler(native_dof_handler));
+
+        nodal_patch_maps[index] =
+          std::make_shared<NodalPatchMap<dim, spacedim>>(patches,
+                                                         bboxes,
+                                                         nodal_coordinates);
+      }
+    return *nodal_patch_maps[index];
+  }
 
   // instantiations
   template class NodalInteraction<NDIM - 1, NDIM>;
