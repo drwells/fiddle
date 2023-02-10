00a22e189fbad1d4a09ffbc72966e5333f8950dd
David Wells <drwells@email.unc.edu>
2023-02-10 16:35:04 -0500

    Multiple surface mesh features.
    I'm too lazy to disentangle these.
    
    1. The previous version of this function forced generation of a convex
       hull - this is not quite what we want in practice. In its place use
       PSLG (vertices and edges) and implement a simple algorithm for
       finding edges from a set of vertices.
    
    2. Implement a function which smoothly displaces the boundary of the
       mesh to specified coordinates.
    
    Amusingly the new PSLG algorithm is robust enough that it fixes the
    first two surface mesh tests.

--- include/fiddle/grid/surface_tria.h
+++ include/fiddle/grid/surface_tria.h
@@ -22,7 +22,8 @@ namespace fdl
       AdditionalData()
         : min_angle(30.0)
         , target_element_area(std::numeric_limits<double>::max())
-        , place_additional_boundary_vertices(true)
+        , place_additional_boundary_vertices(false)
+        , apply_fixup_routines(false)
       {}
 
       /**
@@ -40,31 +41,65 @@ namespace fdl
 
       /**
        * Whether or not additional vertices on the boundary (called Steiner
-       * points) should be placed. Defaults to true.
+       * points) should be placed. Defaults to false.
        */
       bool place_additional_boundary_vertices;
+
+      /**
+       * Whether or not the resulting mesh should be postprocessed by deleting
+       * duplicate or unused vertices. This defaults to false so that the
+       * vertex ordering is the same between the created triangulation and the
+       * input vertices.
+       */
+      bool apply_fixup_routines;
     };
   } // namespace Triangle
 
   /**
-   * Triangulate a surface described by a convex hull.
+   * Triangulate a surface described by a set of vertices.
+   *
+   * To create a non-convex mesh this algorithm must determine edges from @p
+   * vertices. Edges are detected in a three-step process:
+   *
+   * 1. Given a previous edge e0, the next adjacent edge e1 is chosen such
+   *    that the new vertex is the closest vertex such that the angle between
+   *    e0 and e1 is less than 180 degrees (i.e., no backtracking) and the new
+   *    vertex is not part of any current edge.
+   * 2. Should that search fail to find a new edge the 180 degree condition is
+   *    removed.
+   * 3. Should that search fail to find a new edge the new vertex condition is
+   *    also removed.
+   *
+   * This algorithm works well with nearly-convex sets but may fail with
+   * star-shaped domains with insufficiently many vertices.
    */
   void
-  triangulate_convex(const std::vector<Point<2>>   &hull_vertices,
-                     Triangulation<2>              &tria,
-                     const Triangle::AdditionalData additional_data = {});
+  triangulate_segments(const std::vector<Point<2>>   &vertices,
+                       Triangulation<2>              &tria,
+                       const Triangle::AdditionalData additional_data = {});
 
   /**
    * Set up a planar mesh which best fits (in the least-squares sense) the three
    * dimensional points.
    *
    * As the output mesh is planar, this algorithm first projects all points onto
-   * a plane and then uses them as a convex hull.
+   * a plane.
    */
   Tensor<1, 3>
   setup_planar_meter_mesh(const std::vector<Point<3>>   &points,
                           Triangulation<2, 3>           &tria,
                           const Triangle::AdditionalData additional_data = {});
+
+  /**
+   * Fit the Triangulation to a new set of boundary vertices. Inner vertices
+   * are displaced according to the minimal surface (i.e, Laplace) equation.
+   *
+   * Only implemented for sequential Triangulations.
+   */
+  template <int dim, int spacedim = dim>
+  void
+  fit_boundary_vertices(const std::vector<Point<spacedim>> &new_vertices,
+                        Triangulation<dim, spacedim>       &tria);
 
 } // namespace fdl
 
--- tests/grid/surface_tria_01.cc
+++ tests/grid/surface_tria_01.cc
@@ -26,7 +26,8 @@ main()
 
     fdl::Triangle::AdditionalData additional_data;
     additional_data.target_element_area = 0.0125;
-    fdl::triangulate_convex(vertices, tria, additional_data);
+    additional_data.place_additional_boundary_vertices = true;
+    fdl::triangulate_segments(vertices, tria, additional_data);
 
     GridOut().write_vtk(tria, output);
   }
@@ -44,6 +45,7 @@ main()
 
     fdl::Triangle::AdditionalData additional_data;
     additional_data.target_element_area = 0.0125;
+    additional_data.place_additional_boundary_vertices = true;
     fdl::setup_planar_meter_mesh(vertices, tria, additional_data);
 
     GridOut().write_vtk(tria, output);
@@ -61,6 +63,7 @@ main()
 
     fdl::Triangle::AdditionalData additional_data;
     additional_data.target_element_area = (vertices[1] - vertices[0]).norm() / 8.0;
+    additional_data.place_additional_boundary_vertices = true;
     fdl::setup_planar_meter_mesh(vertices, tria, additional_data);
 
     GridOut().write_vtk(tria, output);
@@ -80,6 +83,47 @@ main()
     additional_data.target_element_area = (vertices[1] - vertices[0]).norm() / 8.0;
     additional_data.place_additional_boundary_vertices = false;
     fdl::setup_planar_meter_mesh(vertices, tria, additional_data);
+
+    GridOut().write_vtk(tria, output);
+  }
+
+  output << "3D example, slanted\n";
+  {
+    Triangulation<2, 3>   tria;
+    std::vector<Point<3>> vertices;
+    const unsigned int n_points = 10;
+    for (unsigned int i = 0; i < n_points; ++i)
+        vertices.emplace_back(std::cos(2.0 * numbers::PI * i / double(n_points)),
+                              std::sin(2.0 * numbers::PI * i / double(n_points)),
+                              std::sin(2.0 * numbers::PI * i / double(n_points)));
+
+    fdl::Triangle::AdditionalData additional_data;
+    additional_data.target_element_area = (vertices[1] - vertices[0]).norm() / 4.0;
+    additional_data.place_additional_boundary_vertices = false;
+    fdl::setup_planar_meter_mesh(vertices, tria, additional_data);
+
+    // this shouldn't do anything since the mesh is already planar
+    fdl::fit_boundary_vertices(vertices, tria);
+
+    GridOut().write_vtk(tria, output);
+  }
+
+  output << "3D example, slanted 2\n";
+  {
+    Triangulation<2, 3>   tria;
+    std::vector<Point<3>> vertices;
+    const unsigned int n_points = 20;
+    for (unsigned int i = 0; i < n_points; ++i)
+        vertices.emplace_back(std::cos(2.0 * numbers::PI * i / double(n_points)),
+                              std::sin(2.0 * numbers::PI * i / double(n_points)),
+                              std::sin(4.0 * numbers::PI * i / double(n_points)));
+
+    fdl::Triangle::AdditionalData additional_data;
+    additional_data.target_element_area = (vertices[1] - vertices[0]).norm() / 4.0;
+    additional_data.place_additional_boundary_vertices = false;
+    fdl::setup_planar_meter_mesh(vertices, tria, additional_data);
+
+    fdl::fit_boundary_vertices(vertices, tria);
 
     GridOut().write_vtk(tria, output);
   }
